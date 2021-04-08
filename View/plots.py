4932957fa3fecb001a7983e552a93c7cfe939670
João Álvaro Ferreira <joaoalvaro.ferreira@hotmail.com>
2021-04-08 06:18:20 +0100

    heatmaps

--- View/plots.py
+++ View/plots.py
@@ -11,6 +11,8 @@ import webbrowser
 
 
 from IPython.display import display
+from folium import plugins
+from folium.plugins import HeatMap
 
 # Import data from EarthPy
 data = et.data.get_data('colorado-flood')
@@ -22,6 +24,7 @@ def plotplot(players):
     LDN_COORDINATES = (40.2, -4)
     myMap = folium.Map(location=LDN_COORDINATES, zoom_start=5)
 
+  
     # Add marker for Boulder, CO
     for player in players:
         folium.Marker(
@@ -30,6 +33,39 @@ def plotplot(players):
             icon=folium.Icon()
         ).add_to(myMap)
 
+
+    myMap.save("map.html")
+    webbrowser.open("map.html")
+
+def heatmap_plot(players):
+
+    LDN_COORDINATES = (40.2, -4)
+    myMap = folium.Map(location=LDN_COORDINATES, zoom_start=5)
+
+    load_file = 'D:\\School\\5oAno\\TESE\Repo\\Pervasive-Game-Balancing\\Resources\\CountryDistributionCSVs\PRT_population.csv'
+    df_acc = pd.read_csv(load_file)
+    # Add marker for Boulder, CO
+    for player in players:
+        folium.Marker(
+            location=[player.PlayerLocationInfo.latitude, player.PlayerLocationInfo.longitude], # coordinates for the marker (Earth Lab at CU Boulder)
+            popup='Gameplay Moment', # pop-up label for the marker
+            icon=folium.Icon()
+        ).add_to(myMap)
+
+
+    df_acc['latitude'] = df_acc['latitude'].astype(float)
+    df_acc['longitude'] = df_acc['longitude'].astype(float)
+
+    # Filter the DF for rows, then columns, then remove NaNs
+    heat_df = df_acc[df_acc['population']>3] # Reducing data size so it runs faster
+    heat_df = heat_df[['latitude', 'longitude']]
+    heat_df = heat_df.dropna(axis=0, subset=['latitude','longitude'])
+
+    # List comprehension to make out list of lists
+    heat_data = [[row['latitude'],row['longitude']] for index, row in heat_df.iterrows()]
+
+    # Plot it on the map
+    HeatMap(heat_data).add_to(myMap)
 
     myMap.save("map.html")
     webbrowser.open("map.html")
